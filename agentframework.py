395198aaba1a85a9a3e7e0e46bcef5675554c5da
kexinsun123 <56235550+kexinsun123@users.noreply.github.com>
2019-11-08 16:05:17 +0000

    Add files via upload

---
+++ agentframework.py
@@ -0,0 +1,26 @@
+import random
+
+class Agent():
+    def __init__ (self, environment):
+        self.y = random.randint(0,99)
+        self.x = random.randint(0,99)
+        self.environment = environment
+        self.store = 0
+            
+    def move(self):
+         if random.random() < 0.5:
+            self.x = (self.x + 1) % 100
+         else:
+            self.x = (self.x - 1) % 100
+
+         if random.random() < 0.5:
+            self.y = (self.y + 1) % 100
+         else:
+            self.y = (self.y - 1) % 100
+            
+    def eat(self): # can you make it eat what is left?
+         if self.environment[self.y][self.x] > 10:
+            self.environment[self.y][self.x] -= 10
+            self.store += 10
+        
+    
